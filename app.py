7b6fe467c463d484c8df135391e1844614538ed3
Andrews Matsubara <andrewsmatsubara@gmail.com>
2024-05-27 20:11:30 -0300

    second class

--- app.py
+++ app.py
@@ -3,4 +3,8 @@ print('Sabor Express\n')
 print('1. Cadastrar restaurante')
 print('2. Listar restaurante')
 print('3. Ativar restaurante')
-print('4. Sair')
+print('4. Sair\n')
+
+chosen_option = input('Escolha uma opção: ')
+
+print('Você escolheu a opção', chosen_option)
