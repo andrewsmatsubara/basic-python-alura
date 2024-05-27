657aca35a0212509bd31d9282a1aa5fbc31c495f
Andrews Matsubara <andrewsmatsubara@gmail.com>
2024-05-27 20:32:35 -0300

    exercise: print method practice

---
+++ exercise-1.py
@@ -0,0 +1,15 @@
+print('Python na Escola de Programação da Alura')
+
+name = 'Andrews'
+age = 28
+
+print(f'Meu nome é {name} e tenho {age} anos')
+
+print('''A
+L
+U
+R
+A''')
+
+pi = 3.14159
+print(f'O valor arrendondado de pi é: {pi}')
