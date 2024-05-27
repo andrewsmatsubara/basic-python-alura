4e75e4aeab1a2e9c2de17a139704902e9de81726
Andrews Matsubara <andrewsmatsubara@gmail.com>
2024-05-27 20:42:41 -0300

    class 4: starting with if statement

--- app.py
+++ app.py
@@ -4,10 +4,19 @@ print('''
 ''')
 
 print('1. Cadastrar restaurante')
-print('2. Listar restaurante')
+print('2. Listar restaurantes')
 print('3. Ativar restaurante')
 print('4. Sair\n')
 
 chosen_option = input('Escolha uma opção: ')
 
 print(f'Você escolheu a opção {chosen_option}')
+
+if chosen_option == 1:
+    print('Cadastrar restaurante')
+elif chosen_option == 2:
+    print('Listar restaurante')
+elif chosen_option == 3:
+    print('Ativar restaurante')
+else:
+    print('Encerrando o programa')
