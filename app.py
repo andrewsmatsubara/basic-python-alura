2b1c0c0d46a386e65a42a9b8e47d9afd48796470
Andrews Matsubara <andrewsmatsubara@gmail.com>
2024-05-27 21:06:26 -0300

    class 7: encapsulate every functionality inside a function

--- app.py
+++ app.py
@@ -1,27 +1,38 @@
 import os
 
-print('''
+def show_app_name():
+    print('''
 █▀ ▄▀█ █▄▄ █▀█ █▀█   █▀▀ ▀▄▀ █▀█ █▀█ █▀▀ █▀ █▀
 ▄█ █▀█ █▄█ █▄█ █▀▄   ██▄ █░█ █▀▀ █▀▄ ██▄ ▄█ ▄█
 ''')
 
-print('1. Registrate restaurant')
-print('2. List restaurant')
-print('3. Activate restaurant')
-print('4. Exit\n')
-
-chosen_option = int(input('Choose an option: '))
-# chosen_option = int(chosen_option)
+def show_options():
+    print('1. Registrate restaurant')
+    print('2. List restaurant')
+    print('3. Activate restaurant')
+    print('4. Exit\n')
 
 def finalize_app():
     os.system('cls')
     print('Shutting app\n')
 
-if chosen_option == 1:
-    print('Registrate restaurant')
-elif chosen_option == 2:
-    print('List restaurant')
-elif chosen_option == 3:
-    print('Activate restaurant')
-else:
-    finalize_app()
+
+def choose_option():
+    chosen_option = int(input('Choose an option: '))
+
+    if chosen_option == 1:
+        print('Registrate restaurant')
+    elif chosen_option == 2:
+        print('List restaurant')
+    elif chosen_option == 3:
+        print('Activate restaurant')
+    else:
+        finalize_app()
+
+def main():
+        show_app_name()
+        show_options()
+        choose_option()
+
+if __name__ == '__main__':
+        main()
