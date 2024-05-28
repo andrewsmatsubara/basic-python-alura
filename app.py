7b3ccbfac3c84d00f07bdb581bab585618dee4e0
Andrews Matsubara <andrewsmatsubara@gmail.com>
2024-05-28 00:05:12 -0300

    class 8: try except

--- app.py
+++ app.py
@@ -16,20 +16,31 @@ def finalize_app():
     os.system('cls')
     print('Shutting app\n')
 
+def invalid_option():
+     print('Invalid option! \n')
+     input('Press enter key to go back to main menu')
+     main()
+
 
 def choose_option():
-    chosen_option = int(input('Choose an option: '))
+    try:
+        chosen_option = int(input('Choose an option: '))
 
-    if chosen_option == 1:
-        print('Registrate restaurant')
-    elif chosen_option == 2:
-        print('List restaurant')
-    elif chosen_option == 3:
-        print('Activate restaurant')
-    else:
-        finalize_app()
+        if chosen_option == 1:
+            print('Registrate restaurant')
+        elif chosen_option == 2:
+            print('List restaurant')
+        elif chosen_option == 3:
+            print('Activate restaurant')
+        elif chosen_option == 4:
+            finalize_app()
+        else:
+            invalid_option()
+    except:
+         invalid_option()
 
 def main():
+        os.system('cls')
         show_app_name()
         show_options()
         choose_option()
