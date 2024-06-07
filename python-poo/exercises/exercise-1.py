ee4849a74ce5d3ab386c54ccfaf5ebfd442455d7
Andrews Matsubara <andrewsmatsubara@gmail.com>
2024-06-07 07:35:59 -0300

    exercise poo 1: create a class and set its attributes

---
+++ python-poo/exercises/exercise-1.py
@@ -0,0 +1,19 @@
+class Music:
+  name = ''
+  artist = ''
+  duration = ''
+
+music_1 = Music()
+music_1.name = 'Music 1'
+music_1.artist = 'Artist 1'
+music_1.duration = '03:00'
+
+music_2 = Music()
+music_2.name = 'Music 2'
+music_2.artist = 'Artist 2'
+music_2.duration = '04:00'
+
+music_3 = Music()
+music_3.name = 'Music 3'
+music_3.artist = 'Artist 3'
+music_3.duration = '05:00'
