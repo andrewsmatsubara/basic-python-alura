e2834463f1ad8a226f7c2b276df0fae3af367d6d
Andrews Matsubara <andrewsmatsubara@gmail.com>
2024-06-07 07:22:58 -0300

    class 1 poo: create first class

---
+++ python-poo/restaurant.py
@@ -0,0 +1,11 @@
+class Restaurant:
+  name = ''
+  category = ''
+  active = False
+
+praca_restaurant = Restaurant()
+pizza_restaurant = Restaurant()
+
+restaurants = [praca_restaurant, pizza_restaurant]
+
+print(restaurants)
